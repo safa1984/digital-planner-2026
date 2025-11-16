165596ade69c9d02d26afb2647cff259a2c23ad0
safa1984 <bsrbsr405@gmail.com>
2025-11-16 14:53:47 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('planner-cache').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => {
+      return resp || fetch(e.request);
+    })
+  );
+});
